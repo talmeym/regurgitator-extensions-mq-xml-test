ccb47224f5dc3727d89b13e9839e625d7efbc913
talmeym <mtalmey@gmail.com>
2016-03-16 16:44:44 +0000

    initial commit for extensions mq xml test

---
+++ src/test/java/com/emarte/regurgitator/test/CreateJmsResponseXmlLoaderTest.java
@@ -0,0 +1,23 @@
+package com.emarte.regurgitator.test;
+
+import com.emarte.regurgitator.core.RegurgitatorException;
+import com.emarte.regurgitator.extensions.mq.CreateJmsResponseXmlLoader;
+import org.junit.Test;
+
+import static com.emarte.regurgitator.core.ConfigurationFile.loadFile;
+
+public class CreateJmsResponseXmlLoaderTest extends XmlLoaderTest {
+	public CreateJmsResponseXmlLoaderTest() {
+		super(new CreateJmsResponseXmlLoader());
+	}
+
+	@Test
+	public void testThis() throws Exception {
+		assertExpectation("classpath:/CreateJmsResponse.xml", "com.emarte.regurgitator.extensions.mq.CreateJmsResponse:[com.emarte.regurgitator.core.CreateResponse:['create-jms-response-1',com.emarte.regurgitator.core.ValueSource:[null,'value'],null],'destination','correlation-id','type']");
+	}
+
+	@Test
+	public void testFullLoad() throws RegurgitatorException {
+		loadFile("classpath:/CreateJmsResponse.xml");
+	}
+}
